05ab9e8baa390aca9cb37b62a44f852d9c9c6949
Jared VandeWater <jaredvande@gmail.com>
2021-06-18 13:26:45 -0600

    initial

---
+++ buglog.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'jaredcoding.us.auth0.com'
+export const audience = 'https://jaredcoding.us.auth0.com/api/v2/'
+export const clientId = '557EneUmJyu3JxGDMNni8OEsUgEjiiH3'
